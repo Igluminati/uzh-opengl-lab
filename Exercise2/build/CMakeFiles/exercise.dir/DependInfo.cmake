90dcb32a92d40dba0db644d21d84bf6207a1c3f7
Igli Kristo <IgliKristo@protonmail.com>
2026-03-19 23:57:34 +0100

    adds exercise 2 sub-directory

---
+++ Exercise2/build/CMakeFiles/exercise.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/Camera.cpp" "CMakeFiles/exercise.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/Camera.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/Cube.cpp" "CMakeFiles/exercise.dir/src/Cube.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/Cube.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/GLApp.cpp" "CMakeFiles/exercise.dir/src/GLApp.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/GLApp.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/GLExercise.cpp" "CMakeFiles/exercise.dir/src/GLExercise.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/GLExercise.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/MultiLine.cpp" "CMakeFiles/exercise.dir/src/MultiLine.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/MultiLine.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/ShaderProgram.cpp" "CMakeFiles/exercise.dir/src/ShaderProgram.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/ShaderProgram.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/Shape.cpp" "CMakeFiles/exercise.dir/src/Shape.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/Shape.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/Torus.cpp" "CMakeFiles/exercise.dir/src/Torus.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/Torus.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/UnitCube.cpp" "CMakeFiles/exercise.dir/src/UnitCube.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/UnitCube.cpp.o.d"
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/src/main.cpp" "CMakeFiles/exercise.dir/src/main.cpp.o" "gcc" "CMakeFiles/exercise.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/steak/Desktop/UZH/Spring 2026/uzh-opengl-lab/Exercise2/build/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
