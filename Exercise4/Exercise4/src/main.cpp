c175c76bb7fbed444fa8450447776fda9c83e14f
Igli Kristo <IgliKristo@protonmail.com>
2026-04-28 11:01:06 +0200

    Set the Exercise 4 window title correctly.

--- Exercise4/Exercise4/src/main.cpp
+++ Exercise4/Exercise4/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char * argv[])
 {
-	cgCourse::GLExample app(glm::uvec2(1200, 1000), "Exercise 3");
+	cgCourse::GLExample app(glm::uvec2(1200, 1000), "Exercise 4");
 	return app.run();
 }
 
